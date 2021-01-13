9694bd8307e79c3d06b6acc6a1c4c738077b82c8
Noubar Topalian <topalian.noubar@gmail.com>
2021-01-13 14:45:22 +0100

    Burger App finished

---
+++ src/Components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,36 @@
+import React,{Component} from 'react';
+import Aux from '../../../hoc/Aux/Aux';
+import Button from '../../UI/Button/Button';
+
+class OrderSummary extends Component{
+
+    componentDidUpdate(){
+        console.log('[OrderSummary] WillUpdate')
+    }
+    
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients)
+        .map(igKey => {
+            return (
+                <li key={igKey}>
+                    <span style={{textTransform: 'capitalize'}}>{igKey}:</span>  {this.props.ingredients[igKey]}
+                </li>
+            );
+        });
+        return(
+            <Aux>
+                <h3>Your order</h3>
+                <p>A delicious Burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p> <strong>Total Price: {this.props.price.toFixed(2)}</strong>  </p>
+                <p>Continue to checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux> 
+        );
+    }
+} 
+
+export default OrderSummary;
